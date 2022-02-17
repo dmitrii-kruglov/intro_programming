317f90c36d4bce927adabbe14f1a46b6ab5d848d
dmitrii-kruglov <79216460+dmitrii-kruglov@users.noreply.github.com>
2022-02-17 16:58:48 +0300

    Семинар 1

--- seminar/01.cpp
+++ seminar/01.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World" << std::endl;
+using namespace std;
+
+void cout_hello() {
+  cout << "Hello World!" << endl;
+}
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+int main() {
+  int c, d;
+  cin >> c >> d;
+  int s = sum(c, d);
+  cout << s << endl;
+  //return 0;
 }
