a9c9fac1d19026d073fae1baf75e75fe390f7b89
Дмитрий Круглов <79216460+dmitrii-kruglov@users.noreply.github.com>
2022-02-19 06:57:50 +0000

    Семинар 2

--- seminar/main.cpp
+++ seminar/main.cpp
@@ -1,16 +1,68 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-void count_hello() {
-  cout << "Hello World!" << endl;
+int last(int digit) {
+  return digit % 10;
 }
 
-int sum(int a, int b) {
-  return a + b;
+int prev_last(int digit) {
+  return digit / 10 % 10;
+}
+
+int first_in_two(int digit) {
+  return digit / 10;
+}
+
+int first_in_three(int digit) {
+  if (digit > 100) {
+    return digit / 100;
+  }
+  else
+    return 0;
+
+}
+
+int first(int digit) {
+  while (digit > 10)
+    digit = digit / 10;
+  return digit;
+}
+
+int count(int digit) {
+  int count = 0;
+  do {
+    digit = digit / 10;
+    count++;
+  } while (digit > 0);
+  return count;
 }
 
 int main() {
-  //count_hello();
-  cout << sum(3, 5) << endl;
+  int a, b;
+  /*
+   cout << "Input 1 int:" << endl;
+   cin >> a;
+   cout << "last:" << last(a) << endl;
+  
+   cout << "Input 1 int:" << endl;
+   cin >> a;
+   cout << "prev_last:" << prev_last(a) << endl;
+  
+   cout << "Input 1 int:" << endl;
+   cin >> a;
+   cout << "first_in_two:" << first_in_two(a) << endl;
+  
+   cout << "Input 1 int:" << endl;
+   cin >> a;
+   cout << "first_in_three:" << first_in_three(a) << endl;
+  */
+  // cout << "Input 1 int:" << endl;
+  // cin >> a;
+  // cout << "first:" << first(a) << endl;  
+
+  cout << "Input 1 int:" << endl;
+  cin >> a;
+  cout << "count:" << count(a) << endl;  
 }
