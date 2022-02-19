eb18ed960ffcab5c5f0fc0bd71d6d9560de9d6c3
Дмитрий Круглов <79216460+dmitrii-kruglov@users.noreply.github.com>
2022-02-19 08:25:49 +0000

    Семинар 02

--- seminar/main.cpp
+++ seminar/main.cpp
@@ -1,22 +1,77 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-  int a, b;
-  cout << "Input 2 ints:" << endl;
-  cin >> a >> b;
-  cout << a + b << endl;
-  cout << a - b << endl;
-  cout << a / b << endl;
-  cout << a * b << endl;
-  cout << a % b << endl;
-  // a = a + 1
-  cout << ++a << endl;
-  cout << a << endl;
-  // a = a - 1
-  cout << a-- << endl;
-  cout << a << endl;
+int last(int digit) {
+  return digit % 10;
+}
 
-  //return 0;
+int prev_last(int digit) {
+  return digit / 10 % 10;
+}
+
+int first_in_two(int digit) {
+  return digit / 10;
+}
+
+int first_in_three(int digit) {
+  if (digit >= 100) {
+    return digit / 100;
+  }
+  else {
+    cout << "Error!!!";
+    return 0;
+  }
+
+}
+
+int first(int digit) {
+  while (digit > 10) 
+    digit = digit / 10;
+  return digit;
+}
+
+int count_v0(int digit) {
+  int count = 0;
+  while (digit > 0) {
+    digit = digit / 10;
+    count++;
+  }
+  return count;
+}
+
+int count(int digit) {
+  int count = 0;
+  do {
+    count++;
+    digit = digit / 10;
+  } while (digit > 0);
+  return count;
+}
+
+int main() {
+  int a;
+
+  cout << "Input 1 int:" << endl;
+  cin >> a;
+  cout << "last:" << last(a) << endl;
+  
+  cout << "Input 1 int:" << endl;
+  cin >> a;
+  cout << "prev_last:" << prev_last(a) << endl;
+  
+  cout << "Input 1 int:" << endl;
+  cin >> a;
+  cout << "first_in_two:" << first_in_two(a) << endl;
+  
+  cout << "Input 1 int:" << endl;
+  cin >> a;
+  cout << "first_in_three:" << first_in_three(a) << endl;
+
+  cout << "Input 1 int:" << endl;
+  cin >> a;
+  cout << "first:" << first(a) << endl;  
+
+  cout << "Input 1 int:" << endl;
+  cin >> a;
+  cout << "count:" << count(a) << endl;  
 }
