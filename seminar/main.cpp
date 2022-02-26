8f1be48aa8f0bc752f970fa19cf4d9730d55b5f8
Kruglov Dmitry <79216460+dmitrii-kruglov@users.noreply.github.com>
2022-02-26 08:12:43 +0000

    Update seminar

--- seminar/main.cpp
+++ seminar/main.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
 using namespace std;
 
-bool right(int row, int coloumn, int in_row, int in_coloumn) {
-  if(row == in_row)
-    return abs(coloumn - in_coloumn) == 1;
-  else if(coloumn == in_coloumn)
-    return abs(row - in_row) == 1;
+struct position {
+  int row;
+  int coloumn;
+};
+
+bool right(position pos_king, position pos_in) {
+  if(pos_king.row == pos_in.row)
+    return abs(pos_king.coloumn - pos_in.coloumn) == 1;
+  else if(pos_king.coloumn == pos_in.coloumn)
+    return abs(pos_king.row - pos_in.row) == 1;
   else 
-    return abs(row - in_row) == 1 && abs(coloumn - in_coloumn) == 1;
+    return abs(pos_king.row - pos_in.row) == 1 
+        && abs(pos_king.coloumn - pos_in.coloumn) == 1;
 }
 
 void task1(){
-  int i, r, c;
+  int i;
+  position pos1;
   cout << "Posotion:" << endl;
-  cin >> r >> c;
+  cin >> pos1.row >> pos1.coloumn;
   for(i = 1; i <= 8; i++){
-    if(right(r, c, r + 1, i))
-        cout << r + 1 << ' ' << i << endl;
+    position new_pos = {pos1.row + 1, i};
+    if(right(pos1, new_pos))
+        cout << new_pos.row << ' ' << new_pos.coloumn << endl;
   }
 }
 
 bool test(){
-  bool test1 = right(5, 8, 6, 7) == true;  
-  bool test2 = right(1, 8, 1, 6) == false;  
-  bool test3 = right(2, 6, 3, 7) == true;
-  bool test4 = right(1, 1, 2, 2) == true;
+  bool test1 = right({5, 8}, {6, 7}) == true;  
+  bool test2 = right({1, 8}, {1, 6}) == false;  
+  bool test3 = right({2, 6}, {3, 7}) == true;
+  bool test4 = right({1, 1}, {2, 2}) == true;
   return test1 && test2 && test3 && test4;
 }
 
