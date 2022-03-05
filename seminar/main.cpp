e7bdeb304453d90c0d36ac175fc43e3be82fda83
Kruglov Dmitry <79216460+dmitrii-kruglov@users.noreply.github.com>
2022-03-05 06:15:35 +0000

    семинар 5 марта

--- seminar/main.cpp
+++ seminar/main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
-#include "chess.h"
-
 using namespace std;
-using namespace chess;
 
-bool test(){
-  bool test1 = chess::in_scope({1, 8}) == true;
-  bool test2 = in_scope({1, 3}) == false;
-  bool test3 = in_scope({22, 9}) == false;
-  return test1 || test2 || test3;
+int sum(int* array, int n){
+  int s = 0;
+  int* el;
+  for(int* i = array; i < &array[n]; i++){
+    s += *i;
+  }
+  return s;
 }
 
 int main(){
-   if(test())
-    task1();
-  else {
-    cerr << "Tests failed!!" << endl;
-    return 1;
-  }
+  int n;
+  cin >> n;
+  
+  int array[n];
+  for(int i = 0; i < n; i++)
+    cin >> array[i];
+
+  cout << "SUM = " << sum(array, n) << endl;
 }
