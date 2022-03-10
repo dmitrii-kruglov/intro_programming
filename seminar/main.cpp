dda534980a9c9c35de3cddc6556338dcd9c2655a
Kruglov Dmitry <79216460+dmitrii-kruglov@users.noreply.github.com>
2022-03-10 15:41:31 +0000

    семинар 10 марта

--- seminar/main.cpp
+++ seminar/main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-  int n, m;
-  cin >> n;
-  cin >> m;
-  
-  int array[n][m];
-  for(int i = 0; i < n; i++)
-    for(int j = 0; j < m; j++)
-      cin >> array[i][j];
 
-  int s = 0;
-  for(int i = 0; i < n; i++)
-    for(int j = 0; j < m; j++){
-      s += array[i][j];
-  }
-  
-  cout << "SUM = " << s << endl;
+int main(){
+  string str1, str2;
+  cin >> str1 >> str2;
+  //getline(cin, str1);
+
+  str1 += ";" + str2;
+  // abcd < abc2
+  if(str1 <= str2)
+    cout << "YES";
+  else
+    cout << "NO";
+
+  //012__aaa34567
+  str1.insert(3, "__");
+  str1.insert(5, str2);
+
+  cout << str1 << endl;
+
+  str2 = str1.substr(4, 3);
+
+  cout << str2 << endl;
 }
