42dfb89440544ce692d9536247e3f76e0d76f219
Kruglov Dmitry <79216460+dmitrii-kruglov@users.noreply.github.com>
2022-03-24 15:22:52 +0000

    семинар 24 марта

--- seminar/main.cpp
+++ seminar/main.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 using namespace std;
 
+int* input_array(int  n){
+  int* array = new int[n];
+  for(int i = 0; i < n; i++)
+    cin >> array[i];
+  return array;
+}
+
+int sum(int** array, int  n, int m){
+  int sum = 0;
+  for(int i = 0; i < n; i++)  
+    for(int j =0; j < m; j++)
+      sum += array[i][j];
+  return sum;
+}
+
+void delet_array(int** arr){  
+  for(int i =0; i < 3; i++)
+    delete[] arr[i];
+  delete[] arr;
+}
 
 int main(){
-  string str1, str2;
-  cin >> str1 >> str2;
-  //getline(cin, str1);
-
-  str1 += ";" + str2;
-  // abcd < abc2
-  if(str1 <= str2)
-    cout << "YES";
-  else
-    cout << "NO";
-
-  //012__aaa34567
-  str1.insert(3, "__");
-  str1.insert(5, str2);
-
-  cout << str1 << endl;
-
-  str2 = str1.substr(4, 3);
-
-  cout << str2 << endl;
+  int* n = new int;
+  *n = 3;
+  int** my_two_massive = new int*[(*n)];
+  for(int i = 0; i < *n; i++)
+    my_two_massive[i] = input_array(7);
+  
+  cout << sum(my_two_massive, *n, 7) << endl;
+  
+  delet_array(my_two_massive);
+  delete n;
 }
