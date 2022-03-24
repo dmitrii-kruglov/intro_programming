ad2933fb76092c6956c40532f67a4c9d79ece78b
Kruglov Dmitry <79216460+dmitrii-kruglov@users.noreply.github.com>
2022-03-24 13:29:14 +0000

    семинар 24 марта

--- seminar/main.cpp
+++ seminar/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 using namespace std;
+// Вводится строка.
+// Разделитель слов – пробел.
+// Продублировать в строке все слова, начинающиеся с «A»
+// вывести
+
+//i - первая буква слова
+string next_word(string full_str, int *i){
+  if(full_str[*i] != ' ' && *i < full_str.size()) {
+    return full_str[(*i)++] + next_word(full_str, i);
+  }
+  return "";
+}
 
 int main() {
-  string str1, str2 = "_ff";
+  string str1;
   cout << "Input string:" << endl;
-  cin >> str1;
+  getline(cin, str1);
   
-  cout << str1.size();
-  
-  // "0123456" => "012"+str2+"3456" => "012_ff3456"
-  cout << str1.insert(3, str2);
-  // "01234567" => "4567"
-  str2 = str1.substr(4);
-  
+  for(int i = 0; i < str1.size(); i++){
+    string word = " " + next_word(str1, &i);
+    if (word[1] == 'A' || word[1] == 'a'){
+      str1.insert(i, word);
+      i += word.size();
+    }      
+  }  
   cout << str1 << endl;
-  cout << str2 << endl;
 }
