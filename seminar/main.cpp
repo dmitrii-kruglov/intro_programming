c0e76ac7d8d8c9ffd9a0c21f4866d32f2a930000
Kruglov Dmitry <79216460+dmitrii-kruglov@users.noreply.github.com>
2022-04-21 12:40:52 +0000

    HW-06

--- seminar/main.cpp
+++ seminar/main.cpp
@@ -1,29 +1,56 @@
 #include <iostream>
 using namespace std;
-// Вводится строка.
-// Разделитель слов – пробел.
-// Продублировать в строке все слова, начинающиеся с «A»
-// вывести
 
-//i - первая буква слова
-string next_word(string full_str, int *i){
-  if(full_str[*i] != ' ' && *i < full_str.size()) {
-    return full_str[(*i)++] + next_word(full_str, i);
-  }
-  return "";
+int sum(int x, int y) 
+{
+    return x+y;
 }
 
-int main() {
-  string str1;
-  cout << "Input string:" << endl;
-  getline(cin, str1);
-  
-  for(int i = 0; i < str1.size(); i++){
-    string word = " " + next_word(str1, &i);
-    if (word[1] == 'A' || word[1] == 'a'){
-      str1.insert(i, word);
-      i += word.size();
-    }      
-  }  
-  cout << str1 << endl;
+int dif(int x, int y)
+{
+    return x-y;
+} 
+
+int* mas_sum(int p, int l[], int h[])
+{
+  int* d = new int[p];
+  for (int i = 0; i <p; i++)
+        d[i] = sum (l[i], h[i]);
+  return d;
+}
+
+int* mas_dif(int p, int l[ ], int h[ ])
+{
+  int* d = new int[p];
+    for (int i = 0; i < p; i++)
+        d[i] = dif(l[ i ], h[ i ]);
+  return d;
+}
+
+int main()
+{
+    int n;
+    char k;
+    cout << "Введите количество чисел, которые могут быть в массиве: ";
+    cin>>n;
+    int a[ n ];
+    int b[ n ];
+    int* c;
+    cout << "Введите числа массива: ";
+    for ( int i = 0; i < n; i++)
+        cin >> a[ i ];
+    cout << "Выберите операцию + или - : ";
+    cin >> k;
+    cout << "Введите числа массива: ";
+    for (int i = 0; i < n; i++)
+        cin >> b[ i ];
+    if (k == '+')
+       c = mas_sum( n, a, b);
+    else
+       c = mas_dif( n, a, b);
+    
+    cout << "Ответ: " << endl;
+    for (int i = 0; i < n; i++)
+      cout << "c[" << i << "]:" << c[i] << endl;
+  delete c;
 }
