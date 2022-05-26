533f6da974b1a1853c98c80849fc0ba3390f9e16
Kruglov Dmitry <79216460+dmitrii-kruglov@users.noreply.github.com>
2022-05-26 15:16:16 +0000

    деревья - семинар 26 мая

--- seminar/main.cpp
+++ seminar/main.cpp
@@ -1,52 +1,22 @@
 #include <iostream>
-using namespace std;
 
-string USER = "user";
-string PASSWORD = "123";
+struct node{
+  int value;
+  node* right;
+  node* left;
+};
 
-float task1(int a, int b){
-  if (b == 0)
-      throw "Деление на 0!";
-  return (float)a / b;
+void addRightNode(node* a, int value){
+  node* b = new node();
+  b->value = value;
+  b->right = nullptr;
+  b->left = nullptr;
+  a->right = b;
 }
-
-bool log_in(string log){
-  string password;
-  for(int i = 0; i < 3; i++){
-    cout << "Введите пароль:";
-    cin >> password;
-    if(log == USER && password == PASSWORD)
-      return true;
-    else
-      cout << "Неудачная попытка!" << endl;
-  }
-  if(log == USER)
-    throw 1;
-  else
-    throw 2;
-}
-
-int main()
-{ 
-  int a1, a2;
-  cout << "Введите два числа:";
-  cin >> a1 >> a2;
-  try{
-     float c =  task1(a1, a2);
-     cout << "Результат деления: " << c;
-  } catch (const char* error){
-    cout << "ERROR: " << error;
-  }
-  string login;
-  cout << "Введите логин:";
-  cin >> login;
-  try{
-      log_in(login);
-      cout << "Успешный вход";
-    } catch(const int i){
-      if(i == 1)
-        cout << "Введен три раза некорректный пароль" << endl;
-      if(i == 2)
-        cout << "Введен некорректный логин" << endl;
-    }
+int main(){
+  node root = {9, nullptr, nullptr};
+  addRightNode(&root, 5);
+  std::cout << root.right->value << std::endl;
+  addRightNode(root.right, 6);
+  std::cout << root.right->right->value << std::endl;
 }
